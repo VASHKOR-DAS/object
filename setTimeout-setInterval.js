bac581e0c68b281dbd2350da1de68e479762a935
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-07-18 00:41:53 +0600

    Ex-1

---
+++ setTimeout-setInterval.js
@@ -0,0 +1,9 @@
+// kichu smy por kono kaj 1 bar e korbe, amon korte chaile setTimeout diye korte hoy
+// Ex-1
+
+// timeout a kono millisecond na dile oita baki sobgulo kaj seshe sathe sathei setTimeout function take execute korbe
+console.log(111);
+setTimeout(function () {
+    console.log(222)
+})
+console.log(333);
